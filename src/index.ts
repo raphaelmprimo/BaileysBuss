9d956974125206ec38df89ded26f151c691e3e6d
Raphael Machado <raphael_primo@live.com>
2025-10-14 10:59:50 -0300

    chore: 🔖 atualizar BAILEYSBUSS_VERSION

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export * from './WABinary/index'
 export * from './WAM/index'
 export * from './WAUSync/index'
 
-export const BAILEYSBUSS_VERSION = '8.10.2025'
+export const BAILEYSBUSS_VERSION = '14.10.2025'
 
 export type WASocket = ReturnType<typeof makeWASocket>
 export { makeWASocket }
